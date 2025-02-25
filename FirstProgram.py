cf6a61fc0275e5980e241d246e5ee8dedf53ba29
afrid <afrid@gmail.com>
2025-02-25 07:44:00 +0530

    The first python program is commited

---
+++ FirstProgram.py
@@ -0,0 +1,11 @@
+#print("hi This is a pycharm")
+def add(a,b):
+    return a+b
+n=int(input("Enter a n  number:"))
+m=int(input("Enter a m number:"))
+result=add(n,m)
+print(f"addtion result:{add(n, m)}")
+
+
+
+

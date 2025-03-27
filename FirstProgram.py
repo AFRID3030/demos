aa5f077081656f2b9f4e2fa7c57c65f4f853af3c
Afrid <afrid44@gmail.com>
2025-03-27 07:50:12 +0530

    HRMS file is commit

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,11 +1,21 @@
 #print("hi This is a pycharm")
 def add(a,b):
     return a+b
-n=int(input("Enter a n  number:"))
-m=int(input("Enter a m number:"))
-result=add(n,m)
-print(f"addtion result:{add(n, m)}")
-
+def subtract(a,b):
+    return a-b
+while True:
+    print("1.ADDITION")
+    print("2.SUBTRACTION")
+    c=input("choose any one number between 1-3:")
+    if c=='3':
+        print("You choose invalid number..!")
+        continue
+    n=int(input("Enter a n  number:"))
+    m=int(input("Enter a m number:"))
+    if c=='1':
+        print(f"addtion result:{add(n,m)}\n")
+    elif c=='2':
+        print(f"Subtraction reslt:{subtract(n,m)}\n")
 
 
 
